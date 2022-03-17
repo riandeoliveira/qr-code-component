c236d394cd3af91d4caf539395fbdc065fd9f8c5
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-17 10:30:17 -0300

    Separate data from rendering

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,15 @@ import {
   Description,
 } from "../styles/Card";
 import img from "../assets/images/qr-code.png";
+import data from "../data/data.json";
 
 export const Card: React.FC = () => {
   return (
     <CardContainer>
       <Image src={img} alt="QR code image" />
       <DescriptionContainer>
-        <Title>Improve your front-end skills by building projects</Title>
-        <Description>
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </Description>
+        <Title>{data.title}</Title>
+        <Description>{data.description}</Description>
       </DescriptionContainer>
     </CardContainer>
   );
