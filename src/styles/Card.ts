f8a2d0cdc8849cc64eaf362ea093b07e1c616792
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-20 16:39:13 -0300

    Update styles

--- src/styles/Card.ts
+++ src/styles/Card.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   background-color: var(--card);
   border-radius: 20px;
-  box-shadow: 0 20px 25px #0000001a;
+  box-shadow: 0 20px 25px var(--card-shadow);
   margin: 20px;
   max-width: 375px;
   padding: 20px;
