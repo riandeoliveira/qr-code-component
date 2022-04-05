b8468d83aeea6283b854837923873575a3b99644
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-05 14:19:59 -0300

    📦 refactor: Update ```Card.ts```

--- src/styles/Card.ts
+++ src/styles/Card.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import theme from "./theme/theme.json";
 
 export const CardContainer = styled.div`
-  background-color: var(--white-color);
+  background-color: ${theme.colors.white};
   border-radius: 20px;
-  box-shadow: 0 20px 25px var(--cod-gray-color);
+  box-shadow: 0 20px 25px ${theme.colors["cod-gray"]};
   margin: 20px;
   max-width: 375px;
   padding: 20px;
@@ -22,12 +23,12 @@ export const DescriptionContainer = styled.div`
 `;
 
 export const Title = styled.h1`
-  color: var(--cloud-burst-color);
+  color: ${theme.colors["cloud-burst"]};
   font-size: 26px;
   margin-bottom: 20px;
 `;
 
 export const Description = styled.p`
-  color: var(--regent-gray-color);
+  color: ${theme.colors["regent-gray"]};
   font-size: 19px;
 `;
