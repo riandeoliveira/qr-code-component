72fd8cad03f13f3b4346e304c5f57c67ee404d31
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-05 14:29:11 -0300

    📦 refactor: Add global styles to ```global-style.ts```

---
+++ src/styles/global/global-style.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "../theme/theme.json";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    box-sizing: border-box;
+    font-family: ${theme.fonts.primary}, ${theme.fonts.secondary};
+    list-style: none;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+
+  html {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+  }
+
+  body {
+    background-color: ${theme.colors["periwinkle-gray"]};
+  }
+`;
+
+export default GlobalStyle;
