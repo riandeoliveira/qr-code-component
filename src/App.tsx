6a05a077fbb077a3b1e19c0063fbf4348d21042b
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-11 16:00:45 -0300

    📦 refactor: Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Card from "./components/Card";
+import { AppBox } from "./styles/App";
+import { qrCode } from "./assets/media";
 
-const App: React.FC = () => {
-  return <Card />;
+const App: React.FC = (): JSX.Element => {
+  return (
+    <AppBox>
+      <img
+        alt="White QR code in the middle of a blue background."
+        src={qrCode}
+      />
+      <div>
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </AppBox>
+  );
 };
 
 export default App;
---
+++ src/styles/App.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import theme from "./theme/theme.json";
+
+export const AppBox = styled.div`
+  background-color: ${theme.colors.white};
+  border-radius: 20px;
+  box-shadow: 0 20px 25px ${theme.colors.cod_gray};
+  margin: 20px;
+  max-width: 375px;
+  padding: 20px;
+  text-align: center;
+
+  img {
+    animation: fade-in 0.5s ease, increase 0.5s ease forwards;
+    border-radius: 13px;
+    width: 0%;
+  }
+
+  div {
+    animation: fade-in 0.5s ease;
+    margin: 20px 0;
+
+    h1 {
+      color: ${theme.colors.cloud_burst};
+      font-size: 26px;
+      margin-bottom: 20px;
+    }
+
+    p {
+      color: ${theme.colors.regent_gray};
+      font-size: 19px;
+    }
+  }
+`;
